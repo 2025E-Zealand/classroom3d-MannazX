ae6e4c6eaa2d2b2c4f621a6d314f73b312594bed
Magnus Sverdrup <magger254@gmail.com>
2025-08-27 22:00:58 +0200

    Included Classroom as Class with Properties and Constructor

---
+++ ClassRoomNet60/ClassRoom.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+	public class ClassRoom
+	{
+        public string ClassName { get; set; }
+		List<Student> StudentList { get; set; }
+		DateTime SemesterStart { get; set; }
+
+        public ClassRoom()
+        {
+             
+        }
+    }
+}
